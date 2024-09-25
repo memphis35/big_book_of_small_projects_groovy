944ce58be7b3675191aaf8c073f64f5d9aa42084
aleksandr <aa.smirnov2@gmail.com>
2024-09-25 09:29:46 -0600

    refactor Bagel script

--- src/main/java/com/example/big_book_groovy/bagel/Bagel.groovy
+++ src/main/java/com/example/big_book_groovy/bagel/Bagel.groovy
@@ -1,6 +1,10 @@
 #!/usr/local/groovy
 package com.example.big_book_groovy.bagel
 
+import java.security.SecureRandom
+import java.util.stream.Collectors
+
+/* execution part */
 println '''Bagel game greets you, traveler!
         These are rules: 
         1. A secret number contains 3 (three) distinctive digits
@@ -9,14 +13,52 @@ println '''Bagel game greets you, traveler!
             2.2 “Fermi” when your guess has a correct digit in the correct place
             2.3 “Bagels” if your guess has no correct digits
         3. You have 10 tries to guess the secret number'''
-def game = new BagelGame()
-def input = 'y'
+
 try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+    def input = 'y'
     while (input == 'y') {
-        def result = game.start(reader)
-        println "Game over and you ${result == 0 ? 'won' : 'lost'}"
-        print 'Game over. Wanna play one more time? (y/any_key): '
+        def result = start_game reader
+        println "The game is over and you have ${result ? 'won' : 'lost'}"
+        print 'Do you want to play one more time? (y/n/any_key): '
         input = reader.readLine()
     }
 }
 
+def start_game(bReader) {
+    def count = 1
+    def isGuessed = false
+    def secret = generate_secret()
+    while (!isGuessed && count < 11) {
+        print "Guess #${count++}: "
+        def userGuess = bReader.readLine()
+        def answer = check_guess secret, userGuess
+        println answer.stream().collect(Collectors.joining(" "))
+        isGuessed = answer.size() == 3 && answer.stream().allMatch { it == 'Fermi' }
+    }
+    isGuessed
+}
+
+def generate_secret() {
+    new SecureRandom().ints(0, 10)
+            .limit(20)
+            .distinct()
+            .limit(3)
+            .mapToObj { it.toString() }
+            .collect(Collectors.joining())
+}
+
+def check_guess(secret, userGuess) {
+    assert userGuess =~ /\d{3}/
+    def result = (0..2).stream()
+            .map { find_match secret, userGuess, it }
+            .filter { Objects.nonNull it }
+            .toList() as List<String>
+    result.isEmpty() ? ['Bagel'] : result
+}
+
+def find_match(secret, input, index) {
+    def currentChar = input.charAt(index)
+    def result = secret.charAt(index) == currentChar ? 'Fermi' : null
+    if (!result) result = secret.indexOf(currentChar as Integer) >= 0 ? 'Pico' : null
+    result
+}
