fba05adcc1e79fd8ecc6f37166dc7a0e72d50d1a
aleksandr <aa.smirnov2@gmail.com>
2024-10-24 20:14:52 -0600

    refactor CarrotBoxGame.groovy

--- src/main/java/com/example/big_book_groovy/carrot/CarrotBoxGame.groovy
+++ src/main/java/com/example/big_book_groovy/carrot/CarrotBoxGame.groovy
@@ -12,6 +12,58 @@ def adjustName = { String name ->
     name.capitalize()
 }
 
+def showClosedBoxes = { p1, p2 ->
+    println """
+        |------|     |------|
+        | GOLD |     | BLUE |
+        |------|     |------|
+        [$p1]     [$p2]
+"""
+}
+
+def showOpenedGoldBoxWithCarrot = { p1, p2 -> println """
+          VVVV
+       \\   VV   /
+        |  ||  |     |------|
+        | GOLD |     | BLUE |
+        |------|     |------|
+        (carrot)
+        [$p1]     [$p2]
+"""
+}
+
+def showOpenedGoldBoxWithoutCarrot = { p1, p2 -> println """
+       \\        /
+        |      |     |------|
+        | GOLD |     | BLUE |
+        |------|     |------|
+        (empty)
+        [$p1]     [$p2]
+"""
+}
+
+def doShowdownGoldBoxWins = { p1, p2 -> println """
+          VVVV
+       \\   VV   /   \\        /
+        |  ||  |     |      |
+        | GOLD |     | BLUE |
+        |------|     |------|
+        (carrot)     (empty)
+        [$p1]     [$p2]
+"""
+}
+
+def doShowdownBlueBoxWins = { p1, p2 -> println """
+                       VVVV
+       \\        /   \\   VV   /
+        |      |     |  ||  |
+        | GOLD |     | BLUE |
+        |------|     |------|
+        (empty)      (carrot)
+        [$p1]     [$p2]
+"""
+}
+
 println 'Carrot in a Box, by Aleksandr Smirnov [aa.smirnov2@gmail.com]'
 
 try (def reader = new BufferedReader(new InputStreamReader(System.in))) {
@@ -21,72 +73,40 @@ try (def reader = new BufferedReader(new InputStreamReader(System.in))) {
     print 'Player 2, enter your name: '
     def playerTwo = reader.readLine().trim()
     def playerTwoAdj = adjustName playerTwo
-    def closedBoxes = """\
-        |------|     |------|
-        | GOLD |     | BLUE |
-        |------|     |------|
-        [$playerOneAdj]     [$playerTwoAdj]
-"""
+
     println 'HERE ARE TWO BOXES:'
-    println closedBoxes
+    showClosedBoxes playerOneAdj, playerTwoAdj
     println "$playerOne, you have a GOLD box in front of you"
     println "$playerTwo, you have a BLUE box in front of you"
     println "$playerTwo, close your eyes. After that, $playerOne, press \"Enter\" to continue"
     reader.readLine()
 
-    def isCarrotInGoldBox = random.nextBoolean()
-
     println "$playerOne, here is what is inside your box: "
-    def openedGoldBoxWithCarrot = """\
-          VVVV
-       \\   VV   /
-        |  ||  |     |------|
-        | GOLD |     | BLUE |
-        |------|     |------|
-        (carrot)
-        [$playerOneAdj]     [$playerTwoAdj]
-"""
-    def openedGoldBoxWithoutCarrot = """\
-       \\        /
-        |      |     |------|
-        | GOLD |     | BLUE |
-        |------|     |------|
-        (empty)
-        [$playerOneAdj]     [$playerTwoAdj]
-"""
-    println isCarrotInGoldBox ? openedGoldBoxWithCarrot : openedGoldBoxWithoutCarrot
+    def isCarrotInGoldBox = random.nextBoolean()
+    if (isCarrotInGoldBox) {
+        showOpenedGoldBoxWithCarrot playerOneAdj, playerTwoAdj
+    } else {
+        showOpenedGoldBoxWithoutCarrot playerOneAdj, playerTwoAdj
+    }
     println "$playerOne, press \"Enter\" when you are ready to continue"
     reader.readLine()
     print(System.lineSeparator() * 50)
-    println "$playerTwo, you now may open your eyes"
 
-    print """$playerOne, say one of the following sentences: 
-    (1) There IS a carrot in my box
-    (2) There IS NOT a carrot in my box
-Press \"Enter\" to continue...
-"""
+    println "$playerTwo, you now may open your eyes"
+    println "$playerOne, say one of the following sentences: "
+    println '   (1) There IS a carrot in my box'
+    println '   (2) There IS NOT a carrot in my box'
+    println 'Press "Enter" to continue...'
     reader.readLine()
+
     print "$playerTwo, do you want to swap boxes with $playerOne? (y/n): "
     def playerTwoInputAnswer = reader.readLine() //TODO validate
     isCarrotInGoldBox = playerTwoInputAnswer == 'y' ? !isCarrotInGoldBox : isCarrotInGoldBox
-    def openedBoxesWithCarrotInGold = """\
-          VVVV
-       \\   VV   /   \\        /
-        |  ||  |     |      |
-        | GOLD |     | BLUE |
-        |------|     |------|
-        (carrot)     (empty)
-        [$playerOneAdj]     [$playerTwoAdj]
-"""
-    def openedBoxesWithCarrotInBlue = """\
-                       VVVV
-       \\        /   \\   VV   /
-        |      |     |  ||  |
-        | GOLD |     | BLUE |
-        |------|     |------|
-        (empty)      (carrot)
-        [$playerOneAdj]     [$playerTwoAdj]
-"""
-    println isCarrotInGoldBox ? openedBoxesWithCarrotInGold : openedBoxesWithCarrotInBlue
-    println "${isCarrotInGoldBox ? playerOne : playerTwo} is the winner!"
+    def winner = isCarrotInGoldBox ? playerOne : playerTwo
+    if (isCarrotInGoldBox) {
+        doShowdownGoldBoxWins playerOneAdj, playerTwoAdj
+    } else {
+        doShowdownBlueBoxWins playerOneAdj, playerTwoAdj
+    }
+    println "$winner is the winner!"
 }
